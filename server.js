4ebb1b726774bddd3380882514c2401870ca54b3
Blueheim <crystalrod0@gmail.com>
2019-04-24 19:29:26 +0200

    add dummy express server for serving index.html

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
