692b7ab86583af6e80d0af737804d5ef9cb94c51
Blueheim <crystalrod0@gmail.com>
2019-04-25 10:44:24 +0200

    add env management

--- src/Chat/ChatConversation.js
+++ src/Chat/ChatConversation.js
@@ -42,7 +42,7 @@ const Chat = props => {
 
   // On mount hook
   useEffect(() => {
-    let sock = io(`https://pacific-reaches-85800.herokuapp.com/chat`);
+    let sock = io(`${process.env.REACT_APP_API_URL}/chat`);
 
     sock.on('connect', () => {
       console.log('connected');
